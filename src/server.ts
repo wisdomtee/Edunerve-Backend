684c9dcaf23753cd4fbe4ce049217af24a7f3d6e
unknown <wisdomtoyin@gmail.com>
2026-04-17 17:31:22 +0100

    fix

--- src/server.ts
+++ src/server.ts
@@ -61,7 +61,6 @@ const allowedOrigins = [
   "http://127.0.0.1:3000",
   "http://localhost:5000",
   "http://127.0.0.1:5000",
-  "https://edunerve-frontend.vercel.app",
   process.env.FRONTEND_URL,
 ].filter(Boolean) as string[]
 
